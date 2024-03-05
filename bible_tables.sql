1e8a6d4da58f1c94d0fecdd176fb4e8f433c15b4
JerpBob <jerupaulbalares@gmail.com>
2024-03-04 19:13:06 -0800

    Changed naming convention and added primary keys and number column to bible_books

--- bible_tables.sql
+++ bible_tables.sql
@@ -6,13 +6,14 @@ create table bible_main (
 	verse SMALLINT,
 	text VARCHAR(700)
 );
-
+-------------------------------------------------------------------------
 -- I used dbeaver and the process of copying the csv file into the table
 -- was not easy, since it could not find my file as it was in WSL 
 -- (dbeaver was on my windows whereas the csv file is on wsl)
 -- If no trouble with file location simply use:
 -- 'COPY bible_main FROM {csv-file location} DELIMITER ',' CSV HEADER;'
 -- **I used the \copy command in psql to acheive the same result
+-------------------------------------------------------------------------
 
 -- Added an id for the verse number relative to the whole Bible ie.
 -- Genesis 1:1 will have id 1 as it is the first verse of the Bible
@@ -21,10 +22,10 @@ alter table bible_main add column id SERIAL primary key;
 -- Creates table bibleBooks having the total number of verses and chapters
 -- for each book of the Bible as well as the average number of verses per
 -- chapter
-create table bibleBooks as
+create table bible_books as
 (
-	with bibleChapters as (
-		select book , COUNT(chapter) as chapterCount
+	with bible_chapters as (
+		select book , COUNT(chapter) as chapter_count
 		from bible_main
 		group by book, chapter
 	)
@@ -32,8 +33,14 @@ create table bibleBooks as
 		b.book, 
 		count(b.chapterCount) chapterCount, 
 		sum(b.chapterCount) verseCount,
-		floor(sum(b.chapterCount) / count(b.chapterCount)) AvgVersePerChapter
-	from bibleChapters b
+		floor(sum(b.chapterCount) / count(b.chapter_count)) avg_verse_per_chapter
+	from bible_chapters b
 	group by b.book
-	order by chapterCount desc
+	order by chapter_count desc
 );
+
+-- Added primary key to biblebooks table and added a new column to insert
+-- the book number in correlation to its placement in the Bible
+-- ** I had to manually update each row to add the book number **
+alter table bible_Books add primary key (book);
+alter table bible_books add book_num SMALLINT;
