415b0f292c6833b304f782049b9412c932315973
JerpBob <jerupaulbalares@gmail.com>
2024-03-13 16:15:30 -0700

    Removed unnecessary queries from bible_queries.sql

--- sql/bible_queries.sql
+++ sql/bible_queries.sql
@@ -29,9 +29,7 @@ order by book_num, chapter
 
 select * from num_word_per_chapter;
 
--- query to find the average and sum of words read per day when one reads:
--- Chapters read are in multiples of 4 because that is the rate at which to 
--- finish the Bible in a year (twice in a year, thrice, etc.)
+-- query to find the average and sum of words read per day when one reads
 -- 4 chapters a day
 with final_q as 
 (
@@ -52,67 +50,4 @@ select
 	fq.sum_num
 from final_q fq
 where fq.row % 4 = 1;
-
--- 8 chapters a day
-with final_q as 
-(
-	select 
-		t.row,
-		t.book, 
-		avg(t.num_words) over (rows between current row and 7 following) as avg_num,
-		sum(t.num_words) over (rows between current row and 7 following) as sum_num
-	from (
-		select *, row_number() over(order by book_num, chapter) as row 
-		from num_word_per_chapter 
-	) t
-)
-select
-	row_number() over (order by fq.row ASC) as day,
-	fq.book,
-	fq.avg_num,
-	fq.sum_num
-from final_q fq 
-where fq.row % 8 = 1;
-
--- 12 chapters a day
-with final_q as 
-(
-	select 
-		t.row,
-		t.book, 
-		avg(t.num_words) over (rows between current row and 11 following) as avg_num,
-		sum(t.num_words) over (rows between current row and 11 following) as sum_num
-	from (
-		select *, row_number() over(order by book_num, chapter) as row 
-		from num_word_per_chapter 
-	) t
-)
-select
-	row_number() over (order by fq.row ASC) as day,
-	fq.book,
-	fq.avg_num,
-	fq.sum_num
-from final_q fq 
-where fq.row % 12 = 1;
-
--- 16 chapters a day
-with final_q as 
-(
-	select 
-		t.row,
-		t.book, 
-		avg(t.num_words) over (rows between current row and 15 following) as avg_num,
-		sum(t.num_words) over (rows between current row and 15 following) as sum_num
-	from (
-		select *, row_number() over(order by book_num, chapter) as row 
-		from num_word_per_chapter 
-	) t
-)
-select
-	row_number() over (order by fq.row ASC) as day,
-	fq.book,
-	fq.avg_num,
-	fq.sum_num
-from final_q fq 
-where fq.row % 16 = 1;
 
